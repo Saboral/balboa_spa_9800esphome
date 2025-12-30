6b8e7924ee7cc11e5ad45ea40d8778e572c03625
Saboral <37920596+Saboral@users.noreply.github.com>
2025-12-30 09:14:13 -0500

    Update sensor.py

--- components/balboa_9800cp/sensor.py
+++ components/balboa_9800cp/sensor.py
@@ -2,22 +2,21 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
-    CONF_ID,
     DEVICE_CLASS_TEMPERATURE,
-    UNIT_CELSIUS,
     STATE_CLASS_MEASUREMENT,
+    UNIT_FAHRENHEIT,
 )
 
 from . import Balboa9800CP
 
 CONF_BALBOA_ID = "balboa_id"
-CONF_WATER_TEMP = "water_temp"
+CONF_WATER_TEMP_F = "water_temp_f"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_BALBOA_ID): cv.use_id(Balboa9800CP),
-        cv.Required(CONF_WATER_TEMP): sensor.sensor_schema(
-            unit_of_measurement=UNIT_CELSIUS,
+        cv.Required(CONF_WATER_TEMP_F): sensor.sensor_schema(
+            unit_of_measurement=UNIT_FAHRENHEIT,
             accuracy_decimals=0,
             device_class=DEVICE_CLASS_TEMPERATURE,
             state_class=STATE_CLASS_MEASUREMENT,
@@ -27,5 +26,5 @@ CONFIG_SCHEMA = cv.Schema(
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_BALBOA_ID])
-    sens = await sensor.new_sensor(config[CONF_WATER_TEMP])
+    sens = await sensor.new_sensor(config[CONF_WATER_TEMP_F])
     cg.add(parent.set_water_temp_sensor(sens))
