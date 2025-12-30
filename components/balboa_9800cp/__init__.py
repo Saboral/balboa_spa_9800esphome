abb16c2b1258a54eddf696c52ba17ddd08528922
Saboral <37920596+Saboral@users.noreply.github.com>
2025-12-30 09:13:56 -0500

    Update __init__.py

--- components/balboa_9800cp/__init__.py
+++ components/balboa_9800cp/__init__.py
@@ -3,8 +3,6 @@ import esphome.config_validation as cv
 from esphome import pins
 from esphome.const import CONF_ID
 
-CODEOWNERS = ["Saboral"]
-
 balboa_ns = cg.esphome_ns.namespace("balboa_9800cp")
 Balboa9800CP = balboa_ns.class_("Balboa9800CP", cg.Component)
 
@@ -12,7 +10,6 @@ CONF_CLK_PIN = "clk_pin"
 CONF_DATA_PIN = "data_pin"
 CONF_CTRL_IN_PIN = "ctrl_in_pin"
 CONF_CTRL_OUT_PIN = "ctrl_out_pin"
-CONF_FRAME_BITS = "frame_bits"
 CONF_GAP_US = "gap_us"
 CONF_PRESS_FRAMES = "press_frames"
 
@@ -23,8 +20,6 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Required(CONF_DATA_PIN): pins.gpio_input_pin_schema,
         cv.Required(CONF_CTRL_IN_PIN): pins.gpio_input_pin_schema,
         cv.Required(CONF_CTRL_OUT_PIN): pins.gpio_output_pin_schema,
-        cv.Optional(CONF_FRAME_BITS, default=76): cv.int_range(min=32, max=128),
-        # doc observed ~12ms gap; 8ms is a safe boundary threshold
         cv.Optional(CONF_GAP_US, default=8000): cv.int_range(min=1000, max=50000),
         cv.Optional(CONF_PRESS_FRAMES, default=6): cv.int_range(min=1, max=30),
     }
@@ -41,6 +36,5 @@ async def to_code(config):
     cout = await cg.gpio_pin_expression(config[CONF_CTRL_OUT_PIN])
 
     cg.add(var.set_pins(clk, data, cin, cout))
-    cg.add(var.set_frame_bits(config[CONF_FRAME_BITS]))
     cg.add(var.set_gap_us(config[CONF_GAP_US]))
     cg.add(var.set_press_frames(config[CONF_PRESS_FRAMES]))
