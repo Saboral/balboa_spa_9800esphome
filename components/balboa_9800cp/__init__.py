0c46a82a7bd3ed7e1b8eaf3442b89f85e12c81a7
Saboral <37920596+Saboral@users.noreply.github.com>
2025-12-30 18:44:06 -0500

    Update __init__.py

--- components/balboa_9800cp/__init__.py
+++ components/balboa_9800cp/__init__.py
@@ -19,12 +19,14 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(Balboa9800CP),
+
             cv.Required(CONF_CLK_PIN): pins.gpio_input_pin_schema,
             cv.Required(CONF_DATA_PIN): pins.gpio_input_pin_schema,
             cv.Required(CONF_CTRL_IN_PIN): pins.gpio_input_pin_schema,
 
-            # IMPORTANT: Treat ctrl_out as an INPUT pin schema so ESPHome doesn't drive it at boot.
-            # We control it manually via pinMode() for safe open-drain/high-Z injection.
+            # IMPORTANT:
+            # ctrl_out is intentionally defined as INPUT so ESPHome does not
+            # drive it at boot. We control it manually in C++ using gpio_set_direction()
             cv.Required(CONF_CTRL_OUT_PIN): pins.gpio_input_pin_schema,
 
             cv.Optional(CONF_GAP_US, default=8000): cv.int_range(min=1000, max=50000),
@@ -35,19 +37,28 @@ CONFIG_SCHEMA = (
 )
 
 async def to_code(config):
+    # Create component instance
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
+    # ESPHome GPIOPin objects (used only outside ISR)
     clk = await cg.gpio_pin_expression(config[CONF_CLK_PIN])
     data = await cg.gpio_pin_expression(config[CONF_DATA_PIN])
-    cin = await cg.gpio_pin_expression(config[CONF_CTRL_IN_PIN])
-    cout = await cg.gpio_pin_expression(config[CONF_CTRL_OUT_PIN])
+    ctrl_in = await cg.gpio_pin_expression(config[CONF_CTRL_IN_PIN])
+    ctrl_out = await cg.gpio_pin_expression(config[CONF_CTRL_OUT_PIN])
 
-    cg.add(var.set_pins(clk, data, cin, cout))
+    cg.add(var.set_pins(clk, data, ctrl_in, ctrl_out))
 
-    # Pass raw GPIO numbers for Arduino attachInterrupt and open-drain injection
-    cg.add(var.set_gpio_numbers(config[CONF_CLK_PIN][CONF_NUMBER],
-                                config[CONF_CTRL_OUT_PIN][CONF_NUMBER]))
+    # Pass raw GPIO numbers for ISR-safe gpio_get_level()
+    cg.add(
+        var.set_gpio_numbers(
+            config[CONF_CLK_PIN][CONF_NUMBER],
+            config[CONF_DATA_PIN][CONF_NUMBER],
+            config[CONF_CTRL_IN_PIN][CONF_NUMBER],
+            config[CONF_CTRL_OUT_PIN][CONF_NUMBER],
+        )
+    )
 
+    # Timing parameters
     cg.add(var.set_gap_us(config[CONF_GAP_US]))
     cg.add(var.set_press_frames(config[CONF_PRESS_FRAMES]))
