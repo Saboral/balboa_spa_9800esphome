ee15935a2c93b7eb5f96504ea027e05423b2e1a1
Saboral <37920596+Saboral@users.noreply.github.com>
2025-12-30 09:15:05 -0500

    Update button.py

--- components/balboa_9800cp/button.py
+++ components/balboa_9800cp/button.py
@@ -1,12 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button
-from esphome.const import CONF_ID
 
 from . import Balboa9800CP
 
 CONF_BALBOA_ID = "balboa_id"
-
 CONF_TEMP_UP = "temp_up"
 CONF_TEMP_DOWN = "temp_down"
 CONF_MODE = "mode"
@@ -17,7 +15,6 @@ BalboaButton = balboa_ns.class_("BalboaButton", button.Button)
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_BALBOA_ID): cv.use_id(Balboa9800CP),
-
         cv.Optional(CONF_TEMP_UP): button.button_schema(BalboaButton),
         cv.Optional(CONF_TEMP_DOWN): button.button_schema(BalboaButton),
         cv.Optional(CONF_MODE): button.button_schema(BalboaButton),
