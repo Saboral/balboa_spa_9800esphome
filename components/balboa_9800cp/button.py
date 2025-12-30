5bfb510943318f71906d55bb9296733e4da83b92
Saboral <37920596+Saboral@users.noreply.github.com>
2025-12-30 08:54:33 -0500

    Update button.py

--- components/balboa_9800cp/button.py
+++ components/balboa_9800cp/button.py
@@ -3,18 +3,20 @@ import esphome.config_validation as cv
 from esphome.components import button
 from esphome.const import CONF_ID
 
-balboa_ns = cg.esphome_ns.namespace("balboa_9800cp")
-Balboa9800CP = balboa_ns.class_("Balboa9800CP", cg.Component)
+from . import Balboa9800CP
 
-BalboaButton = balboa_ns.class_("BalboaButton", button.Button)
+CONF_BALBOA_ID = "balboa_id"
 
 CONF_TEMP_UP = "temp_up"
 CONF_TEMP_DOWN = "temp_down"
 CONF_MODE = "mode"
 
+balboa_ns = cg.esphome_ns.namespace("balboa_9800cp")
+BalboaButton = balboa_ns.class_("BalboaButton", button.Button)
+
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.use_id(Balboa9800CP),
+        cv.GenerateID(CONF_BALBOA_ID): cv.use_id(Balboa9800CP),
 
         cv.Optional(CONF_TEMP_UP): button.button_schema(BalboaButton),
         cv.Optional(CONF_TEMP_DOWN): button.button_schema(BalboaButton),
@@ -23,19 +25,19 @@ CONFIG_SCHEMA = cv.Schema(
 )
 
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_ID])
+    parent = await cg.get_variable(config[CONF_BALBOA_ID])
 
     if CONF_TEMP_UP in config:
         b = await button.new_button(config[CONF_TEMP_UP])
         cg.add(b.set_command(1))
-        cg.add(parent.register_button(b))
+        cg.add(b.set_parent(parent))
 
     if CONF_TEMP_DOWN in config:
         b = await button.new_button(config[CONF_TEMP_DOWN])
         cg.add(b.set_command(2))
-        cg.add(parent.register_button(b))
+        cg.add(b.set_parent(parent))
 
     if CONF_MODE in config:
         b = await button.new_button(config[CONF_MODE])
         cg.add(b.set_command(3))
-        cg.add(parent.register_button(b))
+        cg.add(b.set_parent(parent))
